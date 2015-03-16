54b542d5bbf3ffccb7249dc011bf1245a229c3d2
Yuki KAN <re@pixely.jp>
2015-03-16 22:17:46 +0900

    gulpfile.js: update

--- gulpfile.js
+++ gulpfile.js
@@ -1,38 +1,55 @@
+/*jslint node:true, white:true */
+'use strict';
+
 var fs = require('fs');
 var gulp = require('gulp');
 var include = require('gulp-include');
 var rename = require('gulp-rename');
 var mocha = require('gulp-mocha');
-var typescript = require('gulp-tsc');
-var closureCompiler = require('gulp-closure-compiler');
+var typescript = require('gulp-typescript');
+var uglify = require('gulp-uglify');
+var eol = require('gulp-eol');
+var merge = require('merge2');
 
 gulp.task('include-ts', function () {
     return gulp.src('src/flagrate.ts')
                .pipe(include())
-               .pipe(gulp.dest('.'));
+               .pipe(gulp.dest('dist'));
 });
 
 gulp.task('compile-ts', ['include-ts'], function () {
-    return gulp.src('flagrate.ts')
-               .pipe(typescript({ target: 'ES5', declaration: true }))
-               .pipe(gulp.dest('.'));
+
+    var result = gulp.src('dist/flagrate.ts').pipe(typescript({
+        target: 'ES5',
+        declarationFiles: true
+    }));
+
+    return merge([
+        result.dts.pipe(gulp.dest('dist')),
+        result.js.pipe(gulp.dest('dist'))
+    ]);
 });
 
 gulp.task('wrap-js', ['compile-ts'], function () {
     return gulp.src('src/flagrate.js')
                .pipe(include())
-               .pipe(gulp.dest('.'));
+               .pipe(eol())
+               .pipe(gulp.dest('dist'));
 });
 
 gulp.task('compile-js', ['wrap-js'], function () {
-    gulp.src('flagrate.js')
-        .pipe(closureCompiler())
-        .pipe(rename('flagrate.min.js'))
-        .pipe(gulp.dest('.'));
+    return gulp.src('dist/flagrate.js')
+               .pipe(uglify({ preserveComments: 'some' }))
+               .pipe(eol())
+               .pipe(rename('flagrate.min.js'))
+               .pipe(gulp.dest('dist'));
 });
 
 gulp.task('build', ['compile-js'], function () {
-    fs.unlink('flagrate.ts');
+    //fs.unlink('dist/flagrate.ts');
+    //todo
+    return gulp.src('src/flagrate.css')
+               .pipe(gulp.dest('dist'));
 });
 
 gulp.task('watch', function () {
