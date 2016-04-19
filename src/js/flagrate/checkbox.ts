b57972d50fece24d125876b4ee7d2abd95a38aa4
Yuki KAN <re@pixely.jp>
2016-04-19 13:42:12 +0900

    checkbox.ts: ported

---
+++ src/js/flagrate/checkbox.ts
@@ -0,0 +1,212 @@
+/*
+   Copyright 2016 Webnium
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+'use strict';
+
+import { extendObject, emptyFunction } from './util';
+import { Element, Attribute, Property, FHTMLLabelElement, FHTMLInputElement } from './element';
+
+/*?
+    class flagrate.Checkbox
+**/
+export interface Checkbox extends Instance, FHTMLLabelElement { }
+
+export interface Class {
+    new (option?: Option): Checkbox;
+    prototype: Instance;
+}
+
+export interface Instance {
+    disable(): this;
+    enable(): this;
+    isEnabled(): boolean;
+    isChecked(): boolean;
+    check(): this;
+    uncheck(): this;
+
+    onChange?(e: CheckboxEvent, target: this): void;
+    onCheck?(e: CheckboxEvent, target: this): void;
+    onUncheck?(e: CheckboxEvent, target: this): void;
+
+    _input?: FHTMLInputElement;
+    _checked?: boolean;
+}
+
+export interface Option {
+    /** id attribute. */
+    id?: string;
+
+    /** class attribute. */
+    className?: string;
+
+    /** attribute/value pairs properties. */
+    attribute?: Attribute;
+
+    /** Label text. */
+    label?: string;
+
+    /** icon image URL. */
+    icon?: string;
+
+    /** default is false. */
+    isChecked?: boolean;
+
+    /** default is false. */
+    isFocused?: boolean;
+
+    /** default is false. */
+    isDisabled?: boolean;
+
+    onChange?(e?: CheckboxEvent, target?: Checkbox): void;
+    onCheck?(e?: CheckboxEvent, target?: Checkbox): void;
+    onUncheck?(e?: CheckboxEvent, target?: Checkbox): void;
+}
+
+export interface CheckboxEvent extends Event {
+    targetCheckbox?: Checkbox;
+}
+
+let idCounter = 0;
+
+/*?
+    flagrate.createCheckbox(option)
+    new flagrate.Checkbox(option)
+    - option (Object) - options.
+**/
+function FCheckbox(opt: Option = {}) {
+
+    const id = 'flagrate-checkbox-' + (++idCounter).toString(10);
+
+    this.onChange = opt.onChange || null;
+    this.onCheck = opt.onCheck || null;
+    this.onUncheck = opt.onUncheck || null;
+
+    const attr = opt.attribute || {};
+
+    attr['id'] = opt.id || null;
+    attr['class'] = opt.className || null;
+
+    //create
+    const checkbox = new Element('label', attr).updateText(opt.label) as Checkbox;
+    checkbox.writeAttribute('for', id);
+    extendObject(checkbox, this);
+
+    checkbox.addClassName('flagrate flagrate-checkbox');
+
+    if (opt.icon) {
+        checkbox.addClassName('flagrate-icon');
+        checkbox.setStyle({
+            backgroundImage: `url(${opt.icon})`
+        });
+    }
+
+    checkbox._input = new Element('input', { id: id, type: 'checkbox' });
+    checkbox.insert({ top: new Element() });
+    checkbox.insert({ top: checkbox._input });
+
+    checkbox._input.on('click', e => {
+
+        e.stopPropagation();
+
+        const _e: CheckboxEvent = e;
+        _e.targetCheckbox = checkbox;
+
+        if (checkbox.isChecked() === true) {
+            checkbox.uncheck();
+        } else {
+            checkbox.check();
+        }
+
+        if (checkbox.isChecked() === true) {
+            if (checkbox.onCheck) {
+                checkbox.onCheck(_e, checkbox);
+            }
+        } else {
+            if (checkbox.onUncheck) {
+                checkbox.onUncheck(_e, checkbox);
+            }
+        }
+
+        if (checkbox.onChange) {
+            checkbox.onChange(_e, checkbox);
+        }
+        checkbox.fire('change', { targetCheckbox: checkbox });
+    });
+
+    checkbox._input.on('change', e => e.stopPropagation());
+
+    // state
+    checkbox._checked = false;
+
+    if (opt.isChecked === true) {
+        checkbox.check();
+    }
+    if (opt.isFocused === true) {
+        checkbox.focus();
+    }
+    if (opt.isDisabled === true) {
+        checkbox.disable();
+    }
+
+    return checkbox;
+}
+
+export const Checkbox = FCheckbox as any as Class;
+
+export function createCheckbox(option?: Option): Checkbox {
+    return new Checkbox(option);
+}
+
+Checkbox.prototype = {
+    disable() {
+
+        this.addClassName('flagrate-disabled');
+        this._input.writeAttribute('disabled', true);
+
+        return this;
+    },
+
+    enable() {
+
+        this.removeClassName('flagrate-disabled');
+        this._input.writeAttribute('disabled', false);
+
+        return this;
+    },
+
+    isEnabled() {
+        return !this.hasClassName('flagrate-disabled');
+    },
+
+    isChecked() {
+        return this._checked;
+    },
+
+    check() {
+
+        this._input.checked = true;
+        this._checked = true;
+
+        return this;
+    },
+
+    uncheck() {
+
+        this._input.checked = false;
+        this._checked = false;
+
+        return this;
+    }
+};
