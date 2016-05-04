1245a1f4cf91c6b7ad637f528eb5d72ce52c4418
Yuki KAN <re@pixely.jp>
2016-05-04 18:20:28 +0900

    checkbox.ts: define this of callback functions / fix missing onChange callback

--- src/js/flagrate/checkbox.ts
+++ src/js/flagrate/checkbox.ts
@@ -124,16 +124,20 @@ function FCheckbox(opt: Option = {}) {
 
         if (checkbox.isChecked() === true) {
             if (checkbox.onCheck) {
-                checkbox.onCheck(_e, checkbox);
+                checkbox.onCheck.call(checkbox, _e, checkbox);
             }
 
             checkbox.fire('check', { targetCheckbox: checkbox });
         } else {
             if (checkbox.onUncheck) {
-                checkbox.onUncheck(_e, checkbox);
+                checkbox.onUncheck.call(checkbox, _e, checkbox);
             }
 
             checkbox.fire('uncheck', { targetCheckbox: checkbox });
+        }
+
+        if (checkbox.onChange) {
+            checkbox.onChange.call(checkbox, _e, checkbox);
         }
 
         checkbox.fire('change', { targetCheckbox: checkbox });
